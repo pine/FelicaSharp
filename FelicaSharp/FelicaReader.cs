752723bfc8eff33e7dec4f2c76ba7e27c3a8356b
MIZUNE Pine <pinemz@gmail.com>
2014-06-27 15:55:23 +0900

    Revert "Change to STAThread"
    This reverts commit 2a8e58ad82cba7e9f398ea5081f46ed2c1d43e40.

--- FelicaSharp/FelicaReader.cs
+++ FelicaSharp/FelicaReader.cs
@@ -89,7 +89,6 @@ namespace FelicaSharp
                 if (this.pollingThread == null)
                 {
                     this.pollingThread = new Thread(new ThreadStart(this.Polling));
-                    this.pollingThread.SetApartmentState(ApartmentState.STA);
                     this.pollingThread.Start();
                 }
             }
